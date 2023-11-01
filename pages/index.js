d2e761a4af030b78eead7863f1df9c0c6825e71f
rimji <88181976+rimji@users.noreply.github.com>
2023-11-01 14:24:36 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Dr. Jogi's</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to Dr. Jogi's" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Follow us <code>@drjogis</code>
         </p>
       </main>
 
