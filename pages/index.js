9e3c5df754479bc8505b7913b1e719d565ee690c
rimji <88181976+rimji@users.noreply.github.com>
2023-11-01 19:46:43 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,8 @@ export default function Home() {
 
       <main>
         <Header title="Welcome to Dr.Jogi's" />
-        <p className="description">
-          <code>Desi Panjiri     देसी पंजीरी    ਦੇਸੀ ਪੰਜੀਰੀ    دیسی پنجیری</code><br>
-    Follow us <code>@drjogis</code>
+        <p className="description"> 
+        Follow us <code>@drjogis</code>
         </p>
       </main>
 
