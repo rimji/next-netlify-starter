a4087d77f4ca83a2164c5557aaef239ac4eab6e5
rimji <88181976+rimji@users.noreply.github.com>
2023-11-01 15:08:57 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Dr.Jogi's</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
       <main>
