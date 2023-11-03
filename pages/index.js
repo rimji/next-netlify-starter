c3bf189bfc0174e6320e59ba7884e6fa630ffd73
rimji <88181976+rimji@users.noreply.github.com>
2023-11-03 09:55:25 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,6 @@ export default function Home() {
 
       <main>
         <Header title="Dr.Jogi's" />
-        <a href="https://www.rimsim.com/"><img src="https://user-images.githubusercontent.com/88181976/245754932-8d760de0-b789-439e-93e6-0ce080997326.png" alt="Rimsim" width="90" height="30"></a>
         <p className="description"> 
         <a href="https://www.instagram.com/drjogis" target="_blank" rel="noopener noreferrer"><code>Desi Panjiri for New Mothers</code></a>
         </p>
