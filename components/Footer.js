0e0b6bcaa0a569847225f8a29c2b43cb9261fcb7
rimji <88181976+rimji@users.noreply.github.com>
2023-11-03 08:54:03 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/logo2-drjogis.png" alt="Dr.Jogi's" className={styles.logo} />
+        <img src="/logo2-drjogis3.png" alt="Dr.Jogi's" className={styles.logo} />
       </footer>
     </>
   )
