7e3869888f7d2d9ee60386ac77a53b673c09ac28
rimji <88181976+rimji@users.noreply.github.com>
2023-11-03 11:26:13 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 
       <main>
         <p class="thick"> 
-        DR.JOGI'S
+        Dr.Jogi's
         </p>
         <p className="description"> 
         <a href="https://www.instagram.com/drjogis" target="_blank" rel="noopener noreferrer"><code>Desi Panjiri</code></a>
