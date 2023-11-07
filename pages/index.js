7a0217e5224be7ff1656e9047589f07b0dff9075
rimji <88181976+rimji@users.noreply.github.com>
2023-11-07 21:53:39 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
         </p>
         <p className="description"> 
         <code>
-        Desi Panjiri
+        Panjiri
         </code>
         </p>
       </main>
